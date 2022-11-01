a2562e1924f04ccec04c78556374a2ddc7d71c80
Juan Carlos <jchernandez827@gmail.com>
2022-11-01 12:03:29 -0500

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './components';
+import { Home } from './pages';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
